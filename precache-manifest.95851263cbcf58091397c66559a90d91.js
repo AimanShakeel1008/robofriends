002d93a2beebfe9f6a05b30458b9ec48e3d9bd30
Aiman <aimanshakeel1008@gmail.com>
2020-08-19 14:52:14 +0530

    Updates

---
+++ precache-manifest.95851263cbcf58091397c66559a90d91.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f3c7d791f8e2ccfb86f5e39fe243eae",
+    "url": "/AimanShakeel1008/robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/AimanShakeel1008/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "96ecc1a2963cd0771137",
+    "url": "/AimanShakeel1008/robofriends/static/css/main.2d5d1bf8.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/AimanShakeel1008/robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AimanShakeel1008/robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96ecc1a2963cd0771137",
+    "url": "/AimanShakeel1008/robofriends/static/js/main.13b5e53d.chunk.js"
+  },
+  {
+    "revision": "320940f01baaf5092689",
+    "url": "/AimanShakeel1008/robofriends/static/js/runtime-main.e47f197f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/AimanShakeel1008/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
